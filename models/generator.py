8b6e97786e1c672df786cc51cb6d4141b7406567
Nacho Aristimuño <aristimuno.i97@gmail.com>
2023-02-21 20:08:06 -0300

    Changes to the Generator and GANTrainer classes

--- models/generator.py
+++ models/generator.py
@@ -1,83 +1,34 @@
-import yaml
 import torch
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
-from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 
-class Generator:
+class Generator(torch.nn.Module):
     """Generator class of the GAN for generating answers to the questions given as input
-    along with the context (document)."""
+    along with the context (document).
+    
+    Example input: [CLS] What is the capital of France? [SEP] Paris is the capital of France. [SEP]
+    """
 
-    def __init__(self, config_path: str):
-        with open(config_path) as f:
-            self.config = yaml.safe_load(f)
+    def __init__(self, model_name, tokenizer_name):
+        super(Generator, self).__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-        self.model_name = self.config['model_name']
-        self.tokenizer_name = self.config['tokenizer_name']
-        self.max_length = self.config['max_length']
-        self.batch_size = self.config['batch_size']
-        self.learning_rate = self.config['learning_rate']
-        self.warmup_steps = self.config['warmup_steps']
-        self.epochs = self.config['epochs']
-        self.train_data_path = self.config['train_data_path']
-        self.model_output_path = self.config['model_output_path']
-
-    def load_data(self) -> dict:
-        dataset = load_dataset('json', data_files=self.train_data_path)['train']
-        tokenized_dataset = dataset.map(lambda x: self.tokenize_data(x), batched=True)
-        return tokenized_dataset
-
-    def tokenize_data(self, example):
-        encoded = self.tokenizer.encode_plus(
-            example['context'],
-            example['question'],
-            max_length=self.max_length,
-            truncation='only_second',
-            padding='max_length',
-            return_tensors='pt'
+    def forward(self, input_ids, attention_mask):
+        output = self.model.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            max_length=32,
+            do_sample=True,
+            temperature=0.7,
+            top_p=0.9,
+            num_return_sequences=1,
+            decoder_start_token_id=self.tokenizer.cls_token_id,
+            eos_token_id=self.tokenizer.sep_token_id,
+            pad_token_id=self.tokenizer.pad_token_id,
+            bos_token_id=self.tokenizer.cls_token_id
         )
-        encoded['labels'] = self.tokenizer.encode(
-            example['answer_text'],
-            add_special_tokens=False,
-            truncation=True,
-            max_length=self.max_length
-        )
-        return encoded
-
-    def train(self) -> None:
-        tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_name)
-        model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name)
-
-        train_dataset = self.load_data()
-
-        training_args = Seq2SeqTrainingArguments(
-            output_dir=self.model_output_path,
-            num_train_epochs=self.epochs,
-            per_device_train_batch_size=self.batch_size,
-            learning_rate=self.learning_rate,
-            warmup_steps=self.warmup_steps,
-            prediction_loss_only=True,
-            logging_steps=500,
-            save_steps=1000,
-            overwrite_output_dir=True,
-        )
-
-        trainer = Seq2SeqTrainer(
-            model=model,
-            args=training_args,
-            train_dataset=train_dataset,
-            data_collator=lambda data: {'input_ids': torch.stack([f['input_ids'] for f in data]),
-                                        'attention_mask': torch.stack([f['attention_mask'] for f in data]),
-                                        'decoder_input_ids': torch.stack([f['labels'][:-1] for f in data]),
-                                        'decoder_attention_mask': torch.stack([torch.ones_like(f['labels'][:-1]) for f in data]),
-                                        'labels': torch.stack([f['labels'][1:] for f in data])
-                                        },
-        )
-
-        trainer.train()
-
-        model.save_pretrained(self.model_output_path)
-        tokenizer.save_pretrained(self.model_output_path)
+        return output
 
 
 # Example code
--- train.py
+++ train.py
@@ -1,65 +1,98 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.utils.data as data
+from torch.utils.data import DataLoader
 
-from data_loading import TextDataset
 from generator import Generator
 from discriminator import Discriminator
+from scorer import Scorer
+from dataset import SquadDataset
+
 
 class GANTrainer:
     """Class for handling the training process of the whole GAN"""
 
     def __init__(self, config):
-        self.config = config
-        self.generator = Generator(config)
-        self.discriminator = Discriminator(config)
-        self.train_dataset = TextDataset(config)
-        self.train_loader = None
-        
-    def load_data(self):
-        self.train_loader = data.DataLoader(
-            dataset=self.train_dataset,
-            batch_size=self.config.batch_size,
-            shuffle=True,
-            collate_fn=self.train_dataset.collate_fn
-        )
-        
-    def train(self):
-        criterion = nn.BCELoss()
-        gen_optimizer = optim.Adam(self.generator.parameters(), lr=self.config.gen_learning_rate)
-        dis_optimizer = optim.Adam(self.discriminator.parameters(), lr=self.config.dis_learning_rate)
-        for epoch in range(self.config.num_epochs):
-            for batch_idx, batch_data in enumerate(self.train_loader):
-                # Train the discriminator
-                self.discriminator.train()
-                self.generator.eval()
-                dis_optimizer.zero_grad()
-                real_inputs, real_labels = batch_data
-                real_labels = torch.ones(real_labels.size(0))
-                fake_inputs = self.generator.sample(real_inputs.size(0))
-                fake_labels = torch.zeros(fake_inputs.size(0))
-                dis_inputs = torch.cat((real_inputs, fake_inputs))
-                dis_labels = torch.cat((real_labels, fake_labels))
-                dis_outputs = self.discriminator(dis_inputs)
-                dis_loss = criterion(dis_outputs.squeeze(), dis_labels)
-                dis_loss.backward()
-                dis_optimizer.step()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.batch_size = config["training"]["batch_size"]
+        self.max_epochs = config["training"]["max_epochs"]
+        self.learning_rate = config["training"]["learning_rate"]
 
-                # Train the generator
-                self.generator.train()
-                self.discriminator.eval()
-                gen_optimizer.zero_grad()
-                fake_inputs = self.generator.sample(real_inputs.size(0))
-                gen_outputs = self.discriminator(fake_inputs)
-                gen_labels = torch.ones(gen_outputs.size(0))
-                gen_loss = criterion(gen_outputs.squeeze(), gen_labels)
-                gen_loss.backward()
-                gen_optimizer.step()
-                
-                # Print loss and save models
-                if (batch_idx+1) % self.config.print_every == 0:
-                    print(f"Epoch [{epoch+1}/{self.config.num_epochs}], Batch [{batch_idx+1}/{len(self.train_loader)}], Generator Loss: {gen_loss.item():.4f}, Discriminator Loss: {dis_loss.item():.4f}")
-                if (batch_idx+1) % self.config.save_every == 0:
-                    self.generator.save_model(f"{self.config.model_dir}/generator_{epoch+1}_{batch_idx+1}.pt")
-                    self.discriminator.save_model(f"{self.config.model_dir}/discriminator_{epoch+1}_{batch_idx+1}.pt")
+        self.generator = Generator(config).to(self.device)
+        self.discriminator = Discriminator(config).to(self.device)
+        self.scorer = Scorer(config).to(self.device)
+
+        self.generator_optimizer = optim.Adam(
+            self.generator.parameters(), lr=self.learning_rate
+        )
+        self.discriminator_optimizer = optim.Adam(
+            self.discriminator.parameters(), lr=self.learning_rate
+        )
+        self.scorer_optimizer = optim.Adam(self.scorer.parameters(), lr=self.learning_rate)
+
+        self.criterion = nn.BCELoss()
+
+    def train(self):
+        # Load data
+        train_dataset = SquadDataset(
+            file_path="data/train-v2.0.json",
+            tokenizer=self.generator.tokenizer,
+            max_length=self.generator.max_length,
+        )
+        train_loader = DataLoader(
+            train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=4
+        )
+
+        # Train loop
+        for epoch in range(self.max_epochs):
+            self.generator.train()
+            self.discriminator.train()
+            self.scorer.train()
+
+            for i, batch in enumerate(train_loader):
+                # Set up input data
+                input_ids = batch["input_ids"].to(self.device)
+                attention_mask = batch["attention_mask"].to(self.device)
+                start_positions = batch["start_positions"].to(self.device)
+                end_positions = batch["end_positions"].to(self.device)
+
+                # Train generator
+                generated_ids = self.generator(input_ids, attention_mask)
+                scores = self.scorer(generated_ids, attention_mask)
+                generator_loss = -torch.mean(scores)
+                self.generator_optimizer.zero_grad()
+                generator_loss.backward(retain_graph=True)
+                self.generator_optimizer.step()
+
+                # Train discriminator
+                real_scores = self.discriminator(start_positions, end_positions)
+                fake_scores = self.discriminator(generated_ids, attention_mask)
+                real_labels = torch.ones_like(real_scores)
+                fake_labels = torch.zeros_like(fake_scores)
+                discriminator_loss = (
+                    self.criterion(real_scores, real_labels)
+                    + self.criterion(fake_scores, fake_labels)
+                )
+                self.discriminator_optimizer.zero_grad()
+                discriminator_loss.backward(retain_graph=True)
+                self.discriminator_optimizer.step()
+
+                # Train scorer
+                scores = self.scorer(generated_ids, attention_mask)
+                scorer_loss = self.criterion(scores, real_labels)
+                self.scorer_optimizer.zero_grad()
+                scorer_loss.backward()
+                self.scorer_optimizer.step()
+
+                # Print losses
+                if i % 100 == 0:
+                    print(
+                        f"Epoch {epoch+1}/{self.max_epochs} Batch {i+1}/{len(train_loader)}:"
+                    )
+                    print(f"Generator loss: {generator_loss:.4f}")
+                    print(f"Discriminator loss: {discriminator_loss:.4f}")
+                    print(f"Scorer loss: {scorer_loss:.4f}")
+
+            # Save model after every epoch
+            self.save_model(f"models/generator_{epoch+1}.pt", self.generator)
+            self.save
